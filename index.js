1165f7edcc4426c2b6b168aec07df558682aef89
himanshu mandwal <hkushwah8@gmail.com>
2020-09-16 18:18:35 +0530

    Set the view engine and views

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express=require('express');
 const app=express();
 const port=8000;
-
+//use express router
 app.use('/',require('./routes/index'));//here we could write either ./routes only then also this search for the index.js inside it or either the way it is written './routes/index'
 
+//for setting up our view engine
+app.set('view engine','ejs');
+app.set('views','./views');
 
 
 
