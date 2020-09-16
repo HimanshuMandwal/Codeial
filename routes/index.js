571f9c845e9da63a54743fa234cb02ca893d3e07
himanshu mandwal <hkushwah8@gmail.com>
2020-09-16 18:14:09 +0530

    Add controllers and router for post and  users

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,10 @@ const homeController=require('../controllers/home_controller');
 
 console.log('router is loaded');
 router.get('/',homeController.home);
-
+router.use('/users',require('./users'));//use is used here as this lacalhost:8000/users/ can send a get and a post request it is handled in users.js file
+router.use('/post',require('./posts'));
+//for any further routes  access from here 
+//router.use('./routerName',require('./routerFile'));
 
 
 module.exports=router;
---
+++ routes/posts.js
@@ -0,0 +1,12 @@
+const express =require('express');
+const router=express.Router();
+const postController=require('../controllers/posts_controller');
+
+router.get('/',postController.post); 
+
+
+
+
+
+
+module.exports=router;
