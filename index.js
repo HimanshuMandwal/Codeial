6a9b57aa356d8da788c58daf9923073513287e88
himanshu mandwal <hkushwah8@gmail.com>
2020-12-11 19:36:48 +0530

    added the static files to the layout and as well to individual pages

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const express=require('express');
-const app=express();
+const express = require('express');
+const app = express();
 const port=8000;
 const expressLayouts=require('express-ejs-layouts');
 
-//for using layouts we need to require 'express-ejs-layouts' and write app.use(expressLayouts); just above the routes
+app.use(express.static('./assets'));
+//for using layouts we need to require 'express-ejs-layouts' and write app.use(expressLayouts); just above the routes as the routes are going to use the views that uses this layout
 app.use(expressLayouts);
+
+//to extract the styles and script from sub pages to layout use this two lines
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+
 
 //use express router for different routing
 app.use('/',require('./routes/index'));//here we could write either ./routes only then also this search for the index.js inside it or either the way it is written './routes/index'
