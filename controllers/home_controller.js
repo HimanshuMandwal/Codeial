79f7859aa88124cc84e98e2479c85a48594bcded
himanshu mandwal <hkushwah8@gmail.com>
2021-02-28 22:19:23 +0530

    updated the comment feature code and added some scss

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -6,7 +6,15 @@ const User = require('../models/user');
 module.exports.home = function (req, res) {
   // console.log(req.cookies); for displaying cookies
   // res.cookie('user_id',34); for additing the cookies
-  Post.find({}).populate('user').exec(function(err,posts){ //used the populate function to pre-populate the particular objectIds data
+  Post.find({})
+  .populate('user')
+  .populate({
+    path:'comment',
+    populate: {
+      path:'user'
+    }
+  })
+  .exec(function(err,posts){ //used the populate function to pre-populate the particular objectIds data
     if(err){
       console.log(`Error In getting the post :: ${err}`);
     }
